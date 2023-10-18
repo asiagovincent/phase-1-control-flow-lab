ad12aabc57b1b77b493107a0f659feeac05d169d
asiagovincent11@gmail.com <asiagovincent11@gmail.com>
2023-10-18 15:14:57 +0300

    Done with the lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (someValue > 2500) {
+    return "No can do.";
+  } else {
+    return "This is not a valid distance.";
+  }
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+
+
+
+
+
+function ternaryCheckCity(city) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+ 
 }
